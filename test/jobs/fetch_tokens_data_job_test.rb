cf4b9dc2051564c4243d749865fed68e41b9fc74
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-08-21 20:19:31 +0200

    ✏️ fix job test name

--- test/jobs/fetch_tokens_data_job_test.rb
+++ test/jobs/fetch_tokens_data_job_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class FetchDashboardDataJobTest < ActiveJob::TestCase
+class FetchTokensDataJobTest < ActiveJob::TestCase
   # test "the truth" do
   #   assert true
   # end
